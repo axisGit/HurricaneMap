8be1c3c09dfff90c687dd4e21bb0530fb678ed78
Timothywzy <timothywu09@icloud.com>
2017-04-09 11:32:46 -0500

    google api

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
-function myMap() {
-var mapProp= {
-    center:new google.maps.LatLng(51.508742,-0.120850),
-    zoom:5,
-};
-var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-}
+var map;
+var src = 'http://www.nhc.noaa.gov/storm_graphics/api/AL142016_036adv_CONE.kmz';
+var src2 = 'http://www.nhc.noaa.gov/storm_graphics/api/AL012016_002adv_TRACK.kmz';
+function initMap() {
+        // Create a map object and specify the DOM element for display.
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          scrollwheel: false,
+          zoom: 8
+        });
+
+        var kmlLayer = new google.maps.KmlLayer(src, {
+                  suppressInfoWindows: true,
+                  preserveViewport: false,
+                  map: map
+                });
+                kmlLayer.addListener('click', function(event) {
+                  var content = event.featureData.infoWindowHtml;
+                  var testimonial = document.getElementById('capture');
+                  testimonial.innerHTML = content;
+                });
+      }
