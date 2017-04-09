d94d36466ba50ab9a10b67bd2a73c2085f23cb4d
Timothywzy <timothywu09@icloud.com>
2017-04-09 13:10:40 -0500

    kHz

--- js/main.js
+++ js/main.js
@@ -1,22 +1,38 @@
 var map;
-var src = 'http://www.nhc.noaa.gov/storm_graphics/api/AL142016_036adv_CONE.kmz';
-var src2 = 'http://www.nhc.noaa.gov/storm_graphics/api/AL012016_002adv_TRACK.kmz';
-function initMap() {
-        // Create a map object and specify the DOM element for display.
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          scrollwheel: false,
-          zoom: 8
-        });
 
-        var kmlLayer = new google.maps.KmlLayer(src, {
-                  suppressInfoWindows: true,
-                  preserveViewport: false,
-                  map: map
-                });
-                kmlLayer.addListener('click', function(event) {
-                  var content = event.featureData.infoWindowHtml;
-                  var testimonial = document.getElementById('capture');
-                  testimonial.innerHTML = content;
-                });
-      }
+function initMap() {
+    // Create a map object and specify the DOM element for display.
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        scrollwheel: false,
+        zoom: 8
+    });
+
+    // var src = []
+    // for (var i = 40; i < 47; i++) {
+    //     src[i] = 'http://www.nhc.noaa.gov/storm_graphics/api/AL142016_0' + i + 'adv_TRACK.kmz';
+    //     var kmlLayer = new google.maps.KmlLayer(src[i], {
+    //         suppressInfoWindows: true,
+    //         preserveViewport: false,
+    //         map: map
+    //     });
+    //     kmlLayer.addListener('click', function(event) {
+    //         var content = event.featureData.infoWindowHtml;
+    //         var testimonial = document.getElementById('capture');
+    //         testimonial.innerHTML = content;
+    //     });
+    // }
+
+    var src = 'http://localhost:8000/AL142016_015adv_TRACK.kmz';
+    var kmlLayer = new google.maps.KmlLayer(src, {
+        suppressInfoWindows: true,
+        preserveViewport: false,
+        map: map
+    });
+    kmlLayer.addListener('click', function(event) {
+        var content = event.featureData.infoWindowHtml;
+        var testimonial = document.getElementById('capture');
+        testimonial.innerHTML = content;
+    });
+
+}
